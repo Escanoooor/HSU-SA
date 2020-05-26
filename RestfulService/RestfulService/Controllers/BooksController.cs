8aca72d074f750aa220a9fd20316658aa1ae9a9c
TrangSon <tranghongson@gmail.com>
2020-05-26 08:58:08 +0700

    26/05/2020

---
+++ RestfulService/RestfulService/Controllers/BooksController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using RestfulService.Models;
+using RestfulService.DAL;
+
+namespace RestfulService.Controllers
+{
+    public class BooksController : ApiController
+    {
+        [HttpGet]
+        [Route("api/books")]
+        public List<Book> GetAll()
+        {
+            List<Book> books = new BookDAO().SelectAll();
+            return books;
+        }
+
+        [HttpGet]
+        [Route("api/books/{code}")]
+        public Book GetDetails(int code)
+        {
+            Book book = new BookDAO().SelectByCode(code);
+            return book;
+        }
+
+        [HttpPost]
+        [Route("api/books")]
+        public Book AddNew(Book b)
+        {
+            Book newBook = new BookDAO().Insert(b);
+            return newBook;
+        }
+
+        [HttpPut]
+        [Route("api/books/{code}")]
+        public Book Update(int code, Book b)
+        {
+            Book newBook = new BookDAO().Update(code, b);
+            return newBook;
+        }
+
+        [HttpDelete]
+        [Route("api/books/{code}")]
+        public Book Delete(int code)
+        {
+            Book book = new BookDAO().Delete(code);
+            return book;
+        }
+
+        [HttpGet]
+        [Route("api/books/search/{keyword}")]
+        public List<Book> Search(String keyword)
+        {
+            List<Book> books = new BookDAO().SelectByName(keyword);
+            return books;
+        }
+    }
+}
---
+++ RestfulService/RestfulService/DAL/BookDAO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+using RestfulService.Models;
+
+namespace RestfulService.DAL
+{
+    public class BookDAO
+    {
+        static String strCon = ConfigurationManager.ConnectionStrings["strCon"].ConnectionString;
+        MyDBDataContext db = new MyDBDataContext(strCon);
+
+        public List<Book> SelectAll()
+        {
+            List<Book> books = db.Books.ToList();
+            return books;
+        }
+
+        public Book SelectByCode(int code)
+        {
+            Book book = db.Books.SingleOrDefault(x => x.Code == code);
+            return book;
+        }
+
+        public Book Insert(Book newBook)
+        {
+            try
+            {
+                db.Books.InsertOnSubmit(newBook);
+                db.SubmitChanges();
+                return newBook;
+            } catch
+            {
+                return null;
+            }
+        }
+
+        public Book Update(int code, Book newBook)
+        {
+            try
+            {
+                Book dbBook = db.Books.SingleOrDefault(x => x.Code == code);
+                if (dbBook != null)
+                {
+                    dbBook.Name = newBook.Name;
+                    dbBook.Author = newBook.Author;
+                    dbBook.Price = newBook.Price;
+                    db.SubmitChanges();
+                }
+                return dbBook;
+            } catch
+            {
+                return null;
+            }
+        }
+
+        public Book Delete(int code)
+        {
+            try
+            {
+                Book dbBook = db.Books.SingleOrDefault(x => x.Code == code);
+                if (dbBook != null)
+                {
+                    db.Books.DeleteOnSubmit(dbBook);
+                    db.SubmitChanges();
+                }
+                return dbBook;
+            } catch
+            {
+                return null;
+            }
+        }
+
+        public List<Book> SelectByName(String keyword)
+        {
+            List<Book> books = db.Books.Where(b => b.Name.Contains(keyword)).ToList();
+            return books;
+        }
+    }
+}
